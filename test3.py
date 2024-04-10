b9abf3685dc49833eed30409658f6bf09a241bec
sillyButNot <rbqlsquf@konkuk.ac.kr>
2024-04-10 17:12:33 +0900

    similarity #2

--- test3.py
+++ test3.py
@@ -7,15 +7,33 @@ with open("gpt_answer\output_gpt_2_jjs_2.json", "r", encoding="UTF8") as f:
 a = []
 json_writing = {
     "data_number": "",
-    "context": [],
+    "context": "",
     "question": "",
     "real_answer": "",
-    "supporting_fact": [],
+    "supporting_fact": "",
     "gpt_answer": "",
     "message_for_gpt": "",
 }
 
 for i, data in enumerate(json_data):
     json_writing["data_number"] = "number #{}".format(i)
+    json_writing["context"] = data["context"]
     json_writing["question"] = data["question"]
     json_writing["real_answer"] = data["real_answer"]
+    json_writing["supporting_fact"] = data["supporting_fact"]
+    json_writing["gpt_answer"] = data["gpt_answer"]
+    json_writing["message_for_gpt"] = data["message_for_gpt"]
+
+    a.append(json_writing)
+    json_writing = {
+        "data_number": "",
+        "context": "",
+        "question": "",
+        "real_answer": "",
+        "supporting_fact": "",
+        "gpt_answer": "",
+        "message_for_gpt": "",
+    }
+
+with open("output_2.json", "w", encoding="UTF-8") as out_file:
+    json.dump(a, out_file, indent=4, ensure_ascii=False)
